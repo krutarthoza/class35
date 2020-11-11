9e3f580dfae38bc71bcaa04d2968cdaa63daaeca
krutarthoza <66519032+krutarthoza@users.noreply.github.com>
2020-11-11 19:07:29 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,36 @@
+class Form{
+    constructor(){
+       this.input=createInput('name')
+       this.button=createButton('PLAY')
+       this.greeting=createElement('h3')
+    }
+
+    hide(){
+        this.greeting.hide()
+        this.button.hide()
+        this.input.hide()
+        
+    }
+    
+    display(){
+        var title=createElement('h2')
+        title.html("CAR RACING GAME")
+        title.position(130,0);
+        
+        this.input.position(130,160)
+        this.button.position(250,200)
+       
+        this.button.mousePressed (()=>{
+            this.input.hide()
+            this.button.hide()
+            player.name=this.input.value()
+            playerCount=playerCount+1;
+            player.update();
+            player.updateCount(playerCount)
+            this.greeting.html("HELLO "+player.name)
+            this.greeting.position(130,160)
+        })
+    }
+    
+                                                                                        
+}                                                                        
